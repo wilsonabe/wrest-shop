0fa3cad984169bb080243de830e15f2291c78659
wilsonabe <wilson.abe@gmail.com>
2020-12-06 16:19:36 -0500

    Save and Find refactored

--- src/test/java/com/orphynova/restshop/tests/ShopTestsRefactored.java
+++ src/test/java/com/orphynova/restshop/tests/ShopTestsRefactored.java
@@ -1,14 +1,10 @@
 package com.orphynova.restshop.tests;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.orphynova.restshop.models.Products;
 import com.orphynova.restshop.service.ProductService;
-import io.restassured.response.ValidatableResponse;
-import org.apache.http.HttpStatus;
-import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import static io.restassured.RestAssured.given;
+
 
 public class ShopTestsRefactored {
     private String baseuri = "http://localhost:8090";
